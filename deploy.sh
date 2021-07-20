45246d91bf5f82e1f62db38e37f343b0951bf861
Eugene Gwee <eugene.gzj@circles.asia>
2021-07-20 21:22:22 +0800

    fixed docker file name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-docker build -t egcircleslife/multi-client:latest -t egcircleslife/multi-client:$SHA -f ./client/DockerFile ./client
-docker build -t egcircleslife/multi-server:latest -t egcircleslife/multi-server:$SHA -f ./server/DockerFile ./server
-docker build -t egcircleslife/multi-worker:latest -t egcircleslife/multi-worker:$SHA -f ./worker/DockerFile ./worker
+docker build -t egcircleslife/multi-client:latest -t egcircleslife/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t egcircleslife/multi-server:latest -t egcircleslife/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t egcircleslife/multi-worker:latest -t egcircleslife/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push egcircleslife/multi-client:latest
 docker push egcircleslife/multi-server:latest
 docker push egcircleslife/multi-worker:latest
@@ -13,3 +14,6 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=egcircleslife/multi-server:$SHA
 kubectl set image deployments/client-deployment client=egcircleslife/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=egcircleslife/multi-worker:$SHA
+
+
+
