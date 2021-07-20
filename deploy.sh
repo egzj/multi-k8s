202e19768382ee36d9b011373fac2efdbe450950
Eugene Gwee <eugene.gzj@circles.asia>
2021-07-20 18:08:19 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t egcircleslife/multi-client:latest -t egcircleslife/multi-client:$SHA -f ./client/DockerFile ./client
+docker build -t egcircleslife/multi-server:latest -t egcircleslife/multi-server:$SHA -f ./server/DockerFile ./server
+docker build -t egcircleslife/multi-worker:latest -t egcircleslife/multi-worker:$SHA -f ./worker/DockerFile ./worker
+docker push egcircleslife/multi-client:latest
+docker push egcircleslife/multi-server:latest
+docker push egcircleslife/multi-worker:latest
+
+docker push egcircleslife/multi-client:$SHA
+docker push egcircleslife/multi-server:$SHA
+docker push egcircleslife/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=egcircleslife/multi-server:$SHA
+kubectl set image deployments/client-deployment client=egcircleslife/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=egcircleslife/multi-worker:$SHA
