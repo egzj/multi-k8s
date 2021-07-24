81394382615d665f8a4e1455bc22224de3f90d63
Eugene Gwee <eugene.gzj@circles.asia>
2021-07-24 15:06:52 +0800

    edited deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
-docker build -t egcircleslife/multi-client:latest -t egcircleslife/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t egcircleslife/multi-server:latest -t egcircleslife/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t egcircleslife/multi-worker:latest -t egcircleslife/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t egcircleslife/multi-client-k8s:latest -t egcircleslife/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t egcircleslife/multi-server-k8s-pgfix:latest -t egcircleslife/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t egcircleslife/multi-worker-k8s:latest -t egcircleslife/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push egcircleslife/multi-client:latest
-docker push egcircleslife/multi-server:latest
-docker push egcircleslife/multi-worker:latest
+docker push egcircleslife/multi-client-k8s:latest
+docker push egcircleslife/multi-server-k8s-pgfix:latest
+docker push egcircleslife/multi-worker-k8s:latest
 
-docker push egcircleslife/multi-client:$SHA
-docker push egcircleslife/multi-server:$SHA
-docker push egcircleslife/multi-worker:$SHA
+docker push egcircleslife/multi-client-k8s:$SHA
+docker push egcircleslife/multi-server-k8s-pgfix:$SHA
+docker push egcircleslife/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=egcircleslife/multi-server:$SHA
-kubectl set image deployments/client-deployment client=egcircleslife/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=egcircleslife/multi-worker:$SHA
-
-
-
+kubectl set image deployments/server-deployment server=egcircleslife/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=egcircleslife/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=egcircleslife/multi-worker-k8s:$SHA
